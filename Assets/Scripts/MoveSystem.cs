ded53991b151df99f1015b63fe63ae2e3dc8895e
UncleStab <r.f.degen@student.tue.nl>
2023-01-18 21:39:34 +0100

    Add: prefabs for grid and circuit
    Circuits don't really do much right now. Circuits can be dragged and dropped, want to make them snap to a grid location, haven't figured that out yet and I might never because it's not really necessary.

---
+++ Assets/Scripts/MoveSystem.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSystem : MonoBehaviour
+{
+    private bool moving;
+
+    private float startPosX;
+    private float startPosY;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (moving)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, this.gameObject.transform.localPosition.z);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            moving = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        moving = false;
+    }
+}
